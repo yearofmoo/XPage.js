b90ca13067ab4b8699e53d5bc82a33673bb64274
Matias Niemela <matias@yearofmoo.com>
2012-04-21 15:34:43 -0400

    files all separated and demos updated

---
+++ Source/src/XPage.Loaders.js
@@ -0,0 +1,43 @@
+XPage.Loaders = {};
+
+XPage.Loaders.Spinner = {
+
+  init : function(container,options) {
+    this.noFx = !!options.noFx;
+    delete options.noFx;
+    this.spinner = new Spinner(container,options);
+  },
+
+  show : function(container) {
+    this.spinner.show(this.noFx);
+  },
+
+  hide : function(container) {
+    this.spinner.hide(this.noFx);
+  },
+
+  update : function(container) {
+    this.spinner.position();
+  }
+
+};
+
+XPage.Loaders.CursorLoader = {
+
+  init : function(container,options) {
+    this.noFx = !!options.noFx;
+    delete options.noFx;
+    CursorLoader.init(options);
+  },
+
+  show : function(container) {
+    this.noFx ? CursorLoader.show() : CursorLoader.reveal();
+  },
+
+  hide : function(container) {
+    this.noFx ? CursorLoader.hide() : CursorLoader.dissolve();
+  },
+
+  update : function() { }
+
+}
